23ef62920042442b3a37d00eeea2ab6c9f54e698
candeladellamea <89483246+candeladellamea@users.noreply.github.com>
2021-10-29 17:10:20 -0300

    Add files via upload

---
+++ js/lenguaje.js
@@ -0,0 +1,4 @@
+  function aviso () {alert ("Esta es una pagina de recetas")}
+function cartel () {alert ("Esto es un aviso")}
+
+function CambiarFondo () {document.body.style.backgroundcolor: #FD56DE;}
